c7e92b387c748ed223e5cd20f2c089125cd82286
Xanaxhoria777 <nicolantoniomiranda@gmail.com>
2024-07-02 01:03:48 -0500

    Revert "git push origin main"
    This reverts commit 365c96094cba9e0b8ed8645d8f12c7927d8a0c59.

--- miapp/src/components/navbar.jsx
+++ miapp/src/components/navbar.jsx
@@ -1,26 +1,25 @@
-
+import React from 'react'
+import { Button } from 'react-scroll'
 import { BiPhone } from "react-icons/bi";
 import { CgMail } from "react-icons/cg";
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 
 
 const NavBar = () => {
-  const btnLink='mr-8 hover:text-lime-300 hover:text-shadow'
-  const activeLink='mr-8 text-lime-300 text-shadow'
   return (
     <header className="fixed text-lime-500 bg-black/60 	body-font top-0 left-0 right-0 ">
     <div className="container mx-auto flex flex-wrap  md:px-12 flex-col md:flex-row items-center">
       <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto cursor-pointer">
         <ul>
-        <NavLink to="/servicios"className={({isActive}) => isActive?activeLink:btnLink }>Servicios</NavLink>
-        <NavLink to="/sectore" className={({isActive}) => isActive?activeLink:btnLink }>Sectores</NavLink>
-        <NavLink to="/nosotros" className={({isActive}) => isActive?activeLink:btnLink }>Nosotros</NavLink>
-        <NavLink to="/contacto"  className={({isActive}) => isActive?activeLink:btnLink }>Contacto</NavLink>
+        <NavLink to='Servicios'><a className="mr-8 hover:text-lime-300 hover:text-shadow">Servicios</a></NavLink>
+        <a  className="mr-8 hover:text-lime-300 hover:text-shadow">Sectores</a>
+        <a  className="mr-8 hover:text-lime-300 hover:text-shadow">Nosotros</a>
+        <a  className="mr-8 hover:text-lime-300 hover:text-shadow">Contacto</a>
         </ul>
       </nav>
-      <NavLink to="/"  className="flex order-first lg:order-none lg:w-1/5 lg:items-center lg:justify-center mb-4 md:mb-0">
+      <a  className="flex order-first lg:order-none lg:w-1/5 lg:items-center lg:justify-center mb-4 md:mb-0">
         <img className='w-40 h-40' src="../../LOGO 1 EN BLANCO.png" alt="" />
-      </NavLink>
+      </a>
       <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0 cursor-pointer">
      
         <a href='https://wa.me/qr/ASVSS2N76ZQTL1' className="flex lg:items-center mr-8 hover:text-lime-300 hover:text-shadow">
