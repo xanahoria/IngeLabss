e01f237b295ec7927f42745bd8e866bf50776a3d
Leo070203 <lopezlavadoleonardo@hotmail.com>
2024-07-26 14:06:11 -0500

    ServiciosMasPestaña
    Las pestañas faltan para que se vean como el figma

--- miapp/src/Pages/home.jsx
+++ miapp/src/Pages/home.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from 'swiper';
+
 
 
 
@@ -10,7 +10,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
-
+import { Pagination} from 'swiper/modules';
 
 const Home = () => {
   return (
